5b11b470176677e98035de1a48e395f96c9d44ec
Matthew Mogavero <matt.moga@gmail.com>
2018-04-03 18:48:54 -0400

    set up react-router-dom to navigate between Home and Profile components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Route, Link } from 'react-router-dom';
 
 // Components
 import Home from './components/Home';
@@ -9,11 +10,16 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>
-          TEST
-        </h1>
-        <Home />
-        <Profile />
+        <ul>
+          <li>
+            <Link to='/'>Home</Link>
+          </li>
+          <li>
+            <Link to='/profile'>Profile</Link>
+          </li>
+        </ul>
+        <Route path='/' exact component={Home} />
+        <Route path='/profile' exact component={Profile} />
       </div>
     );
   }
