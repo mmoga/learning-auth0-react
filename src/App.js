89252784619d9d886ec20c5ccbf01e3a6801bfa0
Matthew Mogavero <matt.moga@gmail.com>
2018-04-04 19:36:52 -0400

    added to the auth service to get user profile information. set up app to request user information and save that in state

--- src/App.js
+++ src/App.js
@@ -1,48 +1,100 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Route, Link } from 'react-router-dom';
-import Auth from './services/auth';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Link } from "react-router-dom";
+import Auth from "./services/auth";
 
 // Components
-import Home from './components/Home';
-import Profile from './components/Profile';
-import Callback from './components/Callback';
-import PrivateRoute from './components/PrivateRoute';
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import Callback from "./components/Callback";
+import PrivateRoute from "./components/PrivateRoute";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.auth = new Auth();
+    this.state = {
+      profile: {}
+    }
   }
+
+  componentWillMount() {
+    const { getProfile, userProfile } = this.auth;
+    if (!userProfile) {
+      getProfile((err, profile) => {
+        this.setState({
+          profile: profile
+        });
+      });
+    } else {
+      this.setState({
+        profile: userProfile
+      });
+    }
+  }
+
   handleAuthentication = (nextState, replace) => {
     if (/access_token|id_token|error/.test(nextState.location.hash)) {
       this.auth.handleAuthentication();
     }
-  }
+  };
   render() {
     return (
       <div>
         <ul>
           <li>
-            <Link to='/'>Home</Link>
+            <Link to="/">Home</Link>
           </li>
-          <li style={{ display: this.auth.isAuthenticated() ? 'list-item' : 'none' }}>
-            <Link to='/profile'>Profile</Link>
+          <li
+            style={{
+              display: this.auth.isAuthenticated() ? "list-item" : "none"
+            }}
+          >
+            <Link to="/profile">Profile</Link>
           </li>
-          <li style={{ display: this.auth.isAuthenticated() ? 'none' : 'list-item' }}>
-            <button onClick={() => {this.auth.login()}}>Login</button>
+          <li
+            style={{
+              display: this.auth.isAuthenticated() ? "none" : "list-item"
+            }}
+          >
+            <button
+              onClick={() => {
+                this.auth.login();
+              }}
+            >
+              Login
+            </button>
           </li>
-          <li style={{ visibility: this.auth.isAuthenticated() ? 'visible' : 'hidden',
-                        opacity: this.auth.isAuthenticated() ? 1 : 0 }}>
-            <button onClick={() => {this.auth.logout()}}>Logout</button>
+          <li
+            style={{
+              visibility: this.auth.isAuthenticated() ? "visible" : "hidden",
+              opacity: this.auth.isAuthenticated() ? 1 : 0
+            }}
+          >
+            <button
+              onClick={() => {
+                this.auth.logout();
+              }}
+            >
+              Logout
+            </button>
           </li>
         </ul>
-        <Route path='/' exact component={Home} />
-        <PrivateRoute path='/profile' exact component={Profile} auth={this.auth} />
-        <Route path='/callback' exact render={(props) => {
-          this.handleAuthentication(props);
-          return <Callback {...props }/>
-        }} />
+        <Route path="/" exact component={Home} />
+        <PrivateRoute
+          path="/profile"
+          exact
+          component={Profile}
+          auth={this.auth}
+        />
+        <Route
+          path="/callback"
+          exact
+          render={props => {
+            this.handleAuthentication(props);
+            return <Callback {...props} />;
+          }}
+        />
       </div>
     );
   }
--- src/services/auth.js
+++ src/services/auth.js
@@ -1,5 +1,5 @@
 import auth0 from "auth0-js";
-import history from '../history'; 
+import history from "../history";
 
 class Auth {
   auth0 = new auth0.WebAuth({
@@ -8,23 +8,43 @@ class Auth {
     redirectUri: "http://localhost:3000/callback",
     audience: "https://mm-dev.auth0.com/userinfo",
     responseType: "token id_token",
-    scope: "openid"
+    scope: "openid profile"
   });
+
+  getAccessToken() {
+    const accessToken = localStorage.getItem("access_token");
+    if (!accessToken) {
+      return ''; // kind of schlocky :-\
+    }
+    return accessToken;
+  }
+
+  //...
+  getProfile(cb) {
+    let accessToken = this.getAccessToken();
+    this.auth0.client.userInfo(accessToken, (err, profile) => {
+      if (profile) {
+        this.userProfile = profile;
+      }
+      cb(err, profile);
+    });
+  }
 
   constructor() {
     this.login = this.login.bind(this);
     this.logout = this.logout.bind(this);
     this.handleAuthentication = this.handleAuthentication.bind(this);
     this.isAuthenticated = this.isAuthenticated.bind(this);
-  }
+    this.getProfile = this.getProfile.bind(this);
+    }
 
   handleAuthentication() {
     this.auth0.parseHash((err, authResult) => {
       if (authResult && authResult.accessToken && authResult.idToken) {
         this.setSession(authResult);
-        history.replace('/');
+        history.replace("/");
       } else if (err) {
-        history.replace('/');
+        history.replace("/");
         console.log(err);
       }
     });
@@ -32,27 +52,29 @@ class Auth {
 
   setSession(authResult) {
     // Set the time that the Access Token will expire at
-    let expiresAt = JSON.stringify((authResult.expiresIn * 1000) + new Date().getTime());
-    localStorage.setItem('access_token', authResult.accessToken);
-    localStorage.setItem('id_token', authResult.idToken);
-    localStorage.setItem('expires_at', expiresAt);
+    let expiresAt = JSON.stringify(
+      authResult.expiresIn * 1000 + new Date().getTime()
+    );
+    localStorage.setItem("access_token", authResult.accessToken);
+    localStorage.setItem("id_token", authResult.idToken);
+    localStorage.setItem("expires_at", expiresAt);
     // navigate to the home route
-    history.replace('/');
+    history.replace("/");
   }
 
   logout() {
     // Clear Access Token and ID Token from local storage
-    localStorage.removeItem('access_token');
-    localStorage.removeItem('id_token');
-    localStorage.removeItem('expires_at');
+    localStorage.removeItem("access_token");
+    localStorage.removeItem("id_token");
+    localStorage.removeItem("expires_at");
     // navigate to the home route
-    history.replace('/');
+    history.replace("/");
   }
 
   isAuthenticated() {
-    // Check whether the current time is past the 
+    // Check whether the current time is past the
     // Access Token's expiry time
-    let expiresAt = JSON.parse(localStorage.getItem('expires_at'));
+    let expiresAt = JSON.parse(localStorage.getItem("expires_at"));
     return new Date().getTime() < expiresAt;
   }
 
